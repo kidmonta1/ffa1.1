d49c076b270e1cede12323d0bc4631d0cdb03fb1
kidmonta <37140429+kidmonta@users.noreply.github.com>
2019-05-23 01:14:06 +0200

    Initial commit

---
+++ run/win-Install_Dep.bat
@@ -0,0 +1,8 @@
+@echo off
+npm install
+echo.
+echo.
+echo Successfully installed all dependencies!
+echo Please don't touch folder "node-modules".
+echo It is the folder where dependencies, that are reqired to run Multiogar-Edited, are installed.
+pause >nul
